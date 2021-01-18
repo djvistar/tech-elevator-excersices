d91e1c1d732a570e58b35ff297024875959f15ab
VICTOR GWARO <viktah4@gmail.com>
2021-01-18 18:07:52 -0500

    3rd question done

--- 05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
+++ 05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,8 +1,53 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+		
+		
+		 Scanner myScanner = new Scanner(System.in);
+		 
+		 //ask user for to enter number
+		 
+		 System.out.println("Please enter the Fibonacci number:");
+		 String answer = myScanner.nextLine();
+		 
+		 int number = Integer.parseInt(answer);
+		 
+		 
+		 System.out.println("The sequence for "+ number +" is:");
+		 
+		 
+		 int sum, a=0, b = 1;
+		 for (int i = 1; i <= number; ++i) {
+			 
+			 System.out.print(a +" , ");
+
+	            
+	            sum = a + b;
+	            a = b;
+	            b = sum;
+		 }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
