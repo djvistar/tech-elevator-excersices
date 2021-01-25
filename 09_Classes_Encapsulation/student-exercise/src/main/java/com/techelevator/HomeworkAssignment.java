199ceba0f334c4310601cacce3e541aa5a76f589
VICTOR GWARO <viktah4@gmail.com>
2021-01-25 01:14:26 -0500

    2 done

--- 09_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ 09_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -2,4 +2,76 @@ package com.techelevator;
 
 public class HomeworkAssignment {
 
+	
+	private int earnedMarks;
+	private int possibleMarks;
+	private String submitterName;
+	
+	
+	
+	
+	
+	
+	//constructor
+	
+	public HomeworkAssignment (int possibleMarks, String submitterName) {
+		this.possibleMarks = possibleMarks;
+		this.submitterName = submitterName;
+	}
+	
+	//method
+	
+	public String getLetterGrade () {
+		
+		
+		//letterGrade(derived i will use get)) = earnedMarks / possibleMarks;
+		
+		if (((1.0 * earnedMarks / possibleMarks) * 100) >= 90) {
+			
+			return "A";
+			
+		}
+		else if((((double)earnedMarks/ possibleMarks) * 100) >= 80) {
+			return "B";
+		}
+		else if(((1.0 * earnedMarks/ possibleMarks) * 100) >= 70) {
+			return "C";
+		}
+		else if (((1.0 * earnedMarks / possibleMarks) * 100) >= 60) {
+			return "D";
+		} else {
+			return "F";
+		}	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//getters and setters
+	
+
+	public int getEarnedMarks() {
+		return earnedMarks;
+	}
+	public void setEarnedMarks(int earnedMarks) {
+		this.earnedMarks = earnedMarks;
+	}
+	public int getPossibleMarks() {
+		return possibleMarks;
+	}
+	public String getSubmitterName() {
+		return submitterName;
+	}
+	
+	
+	
+	
+	
 }
