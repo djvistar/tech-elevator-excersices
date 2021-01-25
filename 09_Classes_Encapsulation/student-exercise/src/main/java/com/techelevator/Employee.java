fdd15244246657aef49ec9ca6cbce2f5e0b1fd8d
VICTOR GWARO <viktah4@gmail.com>
2021-01-25 01:47:35 -0500

    4 donr

--- 09_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/Employee.java
+++ 09_Classes_Encapsulation/student-exercise/src/main/java/com/techelevator/Employee.java
@@ -2,4 +2,66 @@ package com.techelevator;
 
 public class Employee {
 
+	private int employeeId;
+	private String firstName;
+	private String lastName;
+	private String department;
+	private double annualSalary;
+	
+	
+	//constructor
+	
+	public Employee(int employeeId, String firstName, String lastName, double salary) {
+		
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.annualSalary = salary;		
+	}
+	
+	//method
+	
+	public String getFullName() { 
+		return lastName + ", " + firstName;
+	}
+	
+	
+	
+	
+	public void raiseSalary(double percent) {
+		
+		if (percent > 0.00){
+		annualSalary = annualSalary * (1.0 + (percent/ 100));
+		}
+	}
+	
+	
+	
+	
+	//getters and setters
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public double getAnnualSalary() {
+		return annualSalary;
+	}
+	
+	
+	
 }
