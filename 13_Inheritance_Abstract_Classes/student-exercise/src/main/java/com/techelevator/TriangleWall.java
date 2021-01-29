154415c9718bd17a6c06ffbe3fc65a4fa61b3333
VICTOR GWARO <viktah4@gmail.com>
2021-01-28 23:39:35 -0500

    triangle done

---
+++ 13_Inheritance_Abstract_Classes/student-exercise/src/main/java/com/techelevator/TriangleWall.java
@@ -0,0 +1,51 @@
+package com.techelevator;
+
+public class TriangleWall extends Wall {
+	
+	
+	private int base;
+	private int height;
+	
+	
+	//constructors
+	public TriangleWall(String name, String color, int base, int height) {
+		super(name, color);
+		this.base = base;
+		this.height = height;
+		
+	}
+	
+	
+	
+	
+	
+	//getters
+	
+	public int getBase() {
+		return base;
+	}
+	public int getHeight() {
+		return height;
+	}
+
+
+
+
+
+	@Override
+	public int getArea() {
+	
+		return (base *height)/2;
+	}
+	
+	
+
+	//toString
+	
+	public String toString() {
+		
+		return getName() + " ("+base+"x"+height +") " + "triangle";
+	}
+	
+
+}
