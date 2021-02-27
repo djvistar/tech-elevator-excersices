b045f1d77c95d88b81ef6a0e5d2c4464a4a831d9
VICTOR GWARO <viktah4@gmail.com>
2021-02-27 13:33:12 -0500

    hw push

--- module-2/12_ServerSide_APIs_Part_2/student-exercise/auctions/src/main/java/com/techelevator/auctions/model/Auction.java
+++ module-2/12_ServerSide_APIs_Part_2/student-exercise/auctions/src/main/java/com/techelevator/auctions/model/Auction.java
@@ -5,11 +5,21 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 public class Auction {
-
+   
+	
+	
     private int id;
+    
+    @NotBlank(message = "The field `title` should not be blank.")
     private String title;
+    
+    @NotBlank(message = "The field `description` should not be blank.")
     private String description;
+    
+    @NotBlank(message = "The field `user` should not be blank.")
     private String user;
+    
+    @DecimalMin( value = "1.0", message = "The field `current bid` should be greater than 0.")
     private double currentBid;
 
     public Auction() {
