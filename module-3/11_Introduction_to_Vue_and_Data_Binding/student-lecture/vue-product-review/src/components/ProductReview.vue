719fe196a0579b58c60755adebc7d97d0b2fd172
VICTOR GWARO <viktah4@gmail.com>
2021-03-23 19:54:47 -0400

    pull git commit -m pull git add -A git commit -m pull

---
+++ module-3/11_Introduction_to_Vue_and_Data_Binding/student-lecture/vue-product-review/src/components/ProductReview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class = "main">
+<h2>Product Reviews for {{name}}</h2>
+<p class = "description"> {{description}} </p>
+      </div>
+</template>
+
+<script>
+export default {
+ name: 'product-review',
+ data(){
+     return {
+         pname: 'Cigar Parties For Dummies',
+         description: 'Host and plan squirrel parties'
+         
+     }
+ }
+}
+</script>
+
+<style>
+
+</style>
