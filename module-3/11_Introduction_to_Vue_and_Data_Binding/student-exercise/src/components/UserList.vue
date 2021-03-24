4a4f1d6e4fc9b762a024971c9387ae2780ae7b52
VICTOR GWARO <viktah4@gmail.com>
2021-03-24 00:42:02 -0400

    update

--- module-3/11_Introduction_to_Vue_and_Data_Binding/student-exercise/src/components/UserList.vue
+++ module-3/11_Introduction_to_Vue_and_Data_Binding/student-exercise/src/components/UserList.vue
@@ -11,25 +11,25 @@
     </thead>
     <tbody>
       <tr >
-        <td><input type="text" id="firstNameFilter"/></td>
-        <td><input type="text" id="lastNameFilter"/></td>
-        <td><input type="text" id="usernameFilter"/></td>
-        <td><input type="text" id="emailFilter"/></td>
+        <td><input type="text" id="firstNameFilter" v-model="filter.firstName"/></td>
+        <td><input type="text" id="lastNameFilter" v-model="filter.lastName"/></td>
+        <td><input type="text" id="usernameFilter" v-model="filter.username"/></td>
+        <td><input type="text" id="emailFilter" v-model="filter.emailAddress"/></td>
         <td>
-          <select id="statusFilter">
+          <select id="statusFilter" v-model="filter.status">
             <option value="">Show All</option>
             <option value="Active">Active</option>
             <option value="Disabled">Disabled</option>
           </select>
         </td>
       </tr>
-      <tr  v-for="user in users" v-bind:key = "user"
-      v-bind:status="Disabled">
+      <tr v-for="(user, index) in filteredList" v-bind:key="index" 
+      v-bind:class="{disabled: user.status === 'Disabled'}">
         <td><b>{{user.firstName}}</b></td>
         <td><b>{{user.lastName}}</b></td>
         <td><b>{{user.username}}</b></td>
         <td><b>{{user.emailAddress}}</b></td>
-      {{user.status}}
+       <td>{{ user.status }}</td>
       </tr>
 
       <!-- user listing goes here -->
@@ -42,7 +42,13 @@ export default {
   name: 'user-list',
   data() {
     return {
-      filter: {firstName:'', lastName:'', username:'', emailAddress:'', status},
+       filter: {
+        firstName: '',
+        lastName: '',
+        username: '',
+        emailAddress: '',
+        status: ''
+      },
       users: [
         { firstName: 'John', lastName: 'Smith', username: 'jsmith', emailAddress: 'jsmith@gmail.com', status: 'Active' },
         { firstName: 'Anna', lastName: 'Bell', username: 'abell', emailAddress: 'abell@yahoo.com', status: 'Active' },
@@ -54,12 +60,26 @@ export default {
     }
   },
   computed: {
-  filteredList() {
-    return this.users.filter((user) => {
-      return user.contains(this.filter);
-    });
+    filteredList() {
+      let filteredUsers = this.users;
+      if( this.filter.firstName != "" ) {
+        filteredUsers = filteredUsers.filter(user => user.firstName.toLowerCase().includes(this.filter.firstName.toLowerCase()))
+      }
+      if( this.filter.lastName != "" ) {
+        filteredUsers = filteredUsers.filter(user => user.lastName.toLowerCase().includes(this.filter.lastName.toLowerCase()))
+      }
+      if( this.filter.username != "" ) {
+        filteredUsers = filteredUsers.filter(user => user.username.toLowerCase().includes(this.filter.username.toLowerCase()))
+      }
+      if( this.filter.emailAddress != "" ) {
+        filteredUsers = filteredUsers.filter(user => user.emailAddress.toLowerCase().includes(this.filter.emailAddress.toLowerCase()))
+      }
+      if( this.filter.status != "" ) {
+        filteredUsers = filteredUsers.filter(user => user.status === this.filter.status)
+      }
+      return filteredUsers;
+    }
   }
-}
   
 
 
