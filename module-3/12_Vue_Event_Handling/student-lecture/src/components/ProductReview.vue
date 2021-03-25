5b7a9f73036a60a8a9b523e203a64a9737726938
VICTOR GWARO <viktah4@gmail.com>
2021-03-25 09:00:52 -0400

    pull

--- module-3/12_Vue_Event_Handling/student-lecture/src/components/ProductReview.vue
+++ module-3/12_Vue_Event_Handling/student-lecture/src/components/ProductReview.vue
@@ -36,6 +36,33 @@
       </div>
     </div>
 
+    <form>
+    <div class="form-element">
+        <label for="reviewer">Name:</label>
+        <input id="reviewer" type="text" v-model="newReview.reviewer" />
+    </div>
+    <div class="form-element">
+        <label for="title">Title:</label>
+        <input id="title" type="text" v-model="newReview.title" />
+    </div>
+    <div class="form-element">
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="newReview.rating">
+            <option value="1">1 Star</option>
+            <option value="2">2 Stars</option>
+            <option value="3">3 Stars</option>
+            <option value="4">4 Stars</option>
+            <option value="5">5 Stars</option>
+        </select>
+    </div>
+    <div class="form-element">
+        <label for="review">Review:</label>
+        <textarea id="review" v-model="newReview.review"></textarea>
+    </div>
+    <input type="submit" value="Save">
+    <input type="button" value="Cancel">
+</form>
+
     <div
       class="review"
       v-bind:class="{ favorited: review.favorited }"
@@ -140,6 +167,11 @@ export default {
       return this.reviews.reduce((currentCount, review) => {
         return currentCount + (review.rating === 5);
       }, 0);
+    }
+  },
+  methods: {
+    addNewReview(){
+      
     }
   }
 };
